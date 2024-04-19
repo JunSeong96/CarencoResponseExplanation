658e3e7f8db166ba7c75a4f9976d76dffb195206
JunSeong96 <153894106+JunSeong96@users.noreply.github.com>
2024-04-19 14:02:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const responseTypes = {
-    1: "SUCCESS", 2: "NOT_FOUND", 3: "ALREADY_EXISTS", 4: "REQUIRED",
-    5: "INVALID", 6: "DENIED", 7: "FAILED", 8: "OCCURRED", 9: "DUPLICATED"
+    1: "REQUIRED", 2: "SUCCESS", 3: "ALREADY_EXISTS", 4: "NOT_FOUND",
+    5: "INVALID", 6: "DENIED", 7: "FAILED", 8: "OCCURRED", 9: "DUPLICATED",
+    A: "DIFFERENT"
 };
 
 const actionTypes = {
     1: "RETRIEVE", 2: "CREATE", 3: "UPDATE", 4: "DELETE",
-    5: "PROCESS", 6: "VALIDATE", 7: "REGISTER", 8: "GET", 9: "NOTICE"
+    5: "PROCESS", 6: "VALIDATE", 7: "REGISTER", 8: "GET", 9: "NOTICE",
+    A: "UNKNOWN", B: "LOGIN", C: "RESET", D: "SET", E: "GENERATE",
+    F: "ADD", G: "ENCODE", H: "DECODE", I: "CANCEL", J: "FIND",
+    K: "VERIFY", L: "ACCEPT", M: "DECLINE", N: "EXPIRE", O: "SEARCH"
 };
 
 const categories = {
-    1: "USER", 2: "GROUP", 3: "AUTH", 4: "CLIENT", 5: "SERVER"
+    1: "USER", 2: "GROUP", 3: "AUTH", 4: "CLIENT", 5: "SERVER",
+    6: "FIREBASE", 7: "JWT"
 };
 
 const entityTypes = {
-    10: "INFO", 11: "SCHEDULE", 12: "FOOTPRINT", 13: "POSEESTIMATION", 14: "AVATAR",
+    10: "INFO", 11: "SCHEDULE", 12: "FOOTPRINTS", 13: "POSEESTIMATION", 14: "AVATAR",
     15: "ROLE", 16: "HISTORY", 17: "PARAMETER", 18: "VALIDATION", 19: "AUTHORIZATION",
     20: "REQUEST", 21: "RECORD", 22: "ID", 23: "PASSWORD", 24: "CHECKLIST",
     25: "INVITATION", 26: "DISEASES", 27: "TOKEN", 28: "RESPONSE", 29: "EXCEPTION",
-    30: "ACCOUNT", 31: "MEMBER", 32: "PROFILE"
+    30: "ACCOUNT", 31: "PROFILE", 32: "MEMBER", 33: "LEADER", 34: "EMAIL",
+    35: "PHONE_NUMBER", 36: "PERMISSIONS", 37: "RECOVERY", 38: "CAPACITY",
+    39: "COUNT", 40: "TICKET"
 };
 
 document.getElementById('codeForm').addEventListener('submit', function(event) {
@@ -28,13 +35,13 @@ document.getElementById('codeForm').addEventListener('submit', function(event) {
 });
 
 function interpretCode(code) {
-    const response = responseTypes[parseInt(code.substring(0, 1))] || "";
-    const action = actionTypes[parseInt(code.substring(1, 2))] || "";
-    const category = categories[parseInt(code.substring(2, 3))] || "";
-    const entityTypeCode = parseInt(code.substring(3));
+    const response = responseTypes[parseInt(code.substring(0, 1), 16)] || "";
+    const action = actionTypes[parseInt(code.substring(1, 2), 16)] || "";
+    const category = categories[parseInt(code.substring(2, 3), 16)] || "";
+    const entityTypeCode = parseInt(code.substring(3), 16);
     const entity = entityTypes[entityTypeCode] || "";
 
-    return [response, action, category, entity]
-        .filter(part => part) // Simplified filtering to exclude empty strings
+    return [response, entity, category, action]
+        .filter(part => part) // 빈 문자열 제외
         .join('_');
 }
